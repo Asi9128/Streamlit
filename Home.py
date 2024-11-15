556a8c17202e8f430b28f85e323747416e7f675c
asi9128 <asiwomedavid@gmail.com>
2024-11-15 11:40:51 -1200

    lottie animation embedded

--- Home.py
+++ Home.py
@@ -1,13 +1,15 @@
 import streamlit as st
+import streamlit.components.v1 as com
+#com.iframe("https://lottie.host/embed/1d9c7726-8312-4477-ae0b-7efc13a12654/WKcl6YhXxL.json")
 
 
 def Home_page():
 
     #st.image(r"C:\Users\HP1\OneDrive\Telcho_churn_ml.jpg")
+    com.iframe("https://lottie.host/embed/1d9c7726-8312-4477-ae0b-7efc13a12654/WKcl6YhXxL.json")
+    st.title("Integrating Machine Learning into a GUI with Streamlit :book:")
 
-    st.title('Integrating Machine Learning into a GUI with Streamlit')
-
-    st.title("Telco Churn Classification App")
+    st.title("Telco Churn Classification App :telephone:")
 
 
     st.markdown(""" 
@@ -16,12 +18,12 @@ def Home_page():
 
     st.subheader("Instructions")
     st.markdown("""
-                - Upload a CSV file
-                - Select features for classififcation
-                - Choose a Machine Learning (ML) model from the dropdown
-                - Click on Predict to get the predicted results
-                - The app gives you a report on the performance of the ML model
-                - The app provides a performance report with metrics such as accuracy, precision, recall, f1-score, etc.
+                - Upload a CSV file :o:
+                - Select features for classififcation :star:
+                - Choose a Machine Learning (ML) model from the dropdown :mag_right:
+                - Click on Predict to get the predicted results :magic_wand:
+                - The app gives you a report on the performance of the ML model :chart_with_downwards_trend:
+                - The app provides a performance report with metrics such as accuracy, precision, recall, f1-score, etc. :chart_with_upwards_trend:
     """)
     
     st.header("App features")
@@ -35,6 +37,16 @@ def Home_page():
     st.subheader("User Benefits")
     st.markdown(""" 
                 **Data Driven Approach**: Informed decision making based on the data
+
+                **Streamlined Process**: Streamlined process for churn prediction
+                
+                **Real-time Predictions**: Allows users to input customer data and instantly receive churn predictions, enabling quick interventions to retain customers.
+                
+                **Interactive Insights**: Explore interactive visualizations and dashboards, helping to analyze trends, customer behaviors, and key churn drivers intuitively.
+                
+                **User-friendly Customization**: Provides options to adjust input parameters, filters, and thresholds, tailoring predictions and insights to specific business needs without requiring technical expertise.
+                
+                **Cost-Effective**: Streamlined process and cost-effective approach to churn prediction
      """)
     
 
@@ -65,7 +77,14 @@ def Home_page():
 
 
     st.subheader("Author: David Zodanu")
+    st.markdown("""
+        "Welcome to My Streamlit App! :wave:
 
+     Hi, I'm a passionate data analyst and aspiring AI expert. I specialize in leveraging data-driven solutions to uncover insights and drive impactful decisions.
+     I enjoy working on projects that blend analytics with real-world applications. Explore this app to see how I use data to solve problems and create value.
+     """ )
+    
+    
 # Rating the App
 
     st.header("RATE THIS APP")
@@ -75,21 +94,21 @@ def Home_page():
     if rating == "1":
         st.write("I'm sorry. You have bad taste!😀")    
     elif rating == "2":
-        st.write("Good.You can do better!👏🏽")    
+        st.write("Good but You can do better!👏🏽")    
     elif rating == "3":
         st.write("You are getting there!🥳")    
     elif rating == "4": 
         st.write("You have good taste!🥂")    
     elif rating == "5":
-        st.write("You are amazing!🎉")
+        st.write("You are amazing!🎉:champagne:")
 
 
     st.divider()
     
-    st.write('NEED HELP?')
-    st.write('Email: david.zodanu@azubiafrica.org')
-    st.write('GitHub: https://github.com/Asi9128')
-    st.write('LinkedIn: https://www.linkedin.com/in/david-zodanu/')
-    st.write('Phone: +233 0555 717 949')
+    st.write('NEED HELP:question:')
+    st.write('Email: david.zodanu@azubiafrica.org :email:')
+    st.write('GitHub: https://github.com/Asi9128 :cat:')
+    st.write('LinkedIn: https://www.linkedin.com/in/david-zodanu/ :link:')
+    st.write('Phone: +233 0555 717 949 :phone:')
 
 
