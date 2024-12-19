bf8c012dc4a0ad43cfabd5c2e2516126dd3cf1c9
David Asiwome Zodanu <asiwomedavid@gmail.com>
2024-12-18 23:13:46 -1200

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,30 +2,30 @@ import streamlit as st
 from Home import Home_page
 
 
-#def authentication():
-    #if 'authentication_status' not in st.session_state:
-        #st.session_state['authentication_status'] = False
+def authentication():
+    if 'authentication_status' not in st.session_state:
+        st.session_state['authentication_status'] = False
 
-    #if not st.session_state['authentication_status'] :
+    if not st.session_state['authentication_status'] :
         #login_form()
-    #else:
-        #show_authentication_page()
+    else:
+        show_authentication_page()
 
 
 
-#def login_form():
-    #st.title("Login")
-    #username = st.text_input("Username")
-    #password = st.text_input("Password", type="password")
+def login_form():
+    st.title("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-    #if st.button("Login"):
-        #if username == "admin" and password == "admin":
-            #st.session_state['authentication_status'] = True
-            #st.toast("Login Successfull", icon="✅")
+    if st.button("Login"):
+        if username == "admin" and password == "admin":
+            st.session_state['authentication_status'] = True
+            st.toast("Login Successfull", icon="✅")
             
 
-        #else:
-            #st.error("Username/password is incorrect")
+        else:
+            st.error("Username/password is incorrect")
 
 
 def show_authentication_page():
